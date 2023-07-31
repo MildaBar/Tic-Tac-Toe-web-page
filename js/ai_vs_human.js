5027ac6111a23ea9383c1bacdfef7a1b1ddcde46
Milda Barskiete <stanislovaityte.m@gmail.com>
2023-07-31 13:58:51 +0300

    ai_vs_human.js comments

--- js/ai_vs_human.js
+++ js/ai_vs_human.js
@@ -1,3 +1,4 @@
+// import necessary functions and variables from the script.js file
 import {
   gameOver,
   currentPlayer,
@@ -7,34 +8,46 @@ import {
   gameWinner,
 } from "./script.js";
 
-// START GAME
+// AI vs HUMAN GAME
 export function start_AI_vs_human_game() {
+  // local variables to keep track of game state
   const gridItems = document.querySelectorAll(".grid-item");
   let currentPlayer = 1;
   let gameOver = false;
 
-  // Add an event listener to the "Start Game" button
+  // add an event listener to the "Start Game" button
   const startGameBtn = document.getElementById("start-game-btn");
   startGameBtn.addEventListener("click", () => {
     if (startGameBtn.innerText === "START GAME") {
+      // if the game is starting, change the button text to "RESTART"
       startGameBtn.innerText = "RESTART";
-      currentPlayer = 1;
+      currentPlayer = 1; // reset current player to Player 1
+      gameOver = false; // reset game over state to false
 
       gridItems.forEach((item) => {
+        // clear the grid cells by removing the previous symbols
+        item.innerText = "";
+      });
+
+      // start the game with Player 1 (human) and AI's turn after a delay
+      currentPlayer = 1;
+      gridItems.forEach((item) => {
         item.addEventListener("click", () => {
+          // when a grid item is clicked, check if the game is not over and the cell is empty
           if (!gameOver && item.innerText === "") {
             if (currentPlayer === 1) {
-              item.innerText = "X";
-              currentPlayer = 2; // Switch to AI's turn
-              setTimeout(aiMove, 500); // Automatically trigger AI move after 500ms
+              item.innerText = "X"; // set the cell to "X" for Human Player's turn
+              currentPlayer = 2; // switch to AI's turn
+              setTimeout(aiMove, 500); // automatically trigger AI move after 500ms
             }
 
-            // Check for the winner after each symbol is added
+            // check for the winner after each symbol is added
             const winnerData = gameWinner();
             if (
               winnerData &&
               (winnerData.symbol === "X" || winnerData.symbol === "O")
             ) {
+              // if there's a winner, display the winner message and highlight the winning cells
               const winnerMsgContainer = document.getElementById(
                 "winner-msg-container"
               );
@@ -52,13 +65,15 @@ export function start_AI_vs_human_game() {
                 } wins!`;
               }
 
-              // Highlight the winning cells
+              // highlight the winning cells
               winnerData.cells.forEach((cellIndex) => {
                 gridItems[cellIndex].classList.add("winning-cell");
               });
 
+              // set the game over state to true
               gameOver = true;
             } else if (isGridFull()) {
+              // if the grid is full and there's no winner, it's a tie
               const winnerMsgContainer = document.getElementById(
                 "winner-msg-container"
               );
@@ -67,16 +82,17 @@ export function start_AI_vs_human_game() {
               const winnerMsg = document.getElementById("winner-msg");
               winnerMsg.innerHTML = "It's a tie! The grid is full.";
 
+              // set the game over state to true
               gameOver = true;
             }
 
             if (gameOver) {
-              // Disable further symbol addition once the game is over
+              // disable further symbol addition once the game is over
               gridItems.forEach((item) => {
                 item.removeEventListener("click", start_AI_vs_human_game);
               });
             } else {
-              // Switch player turns if the game is not over
+              // switch player turns if the game is not over
               currentPlayer = currentPlayer === 1 ? 2 : 1;
               playerTurn(currentPlayer);
             }
@@ -84,17 +100,19 @@ export function start_AI_vs_human_game() {
         });
       });
     } else {
-      location.reload();
+      location.reload(); // if the "RESTART" button is clicked, reload the page to start a new game
     }
   });
 }
 
+// AI's move
 function aiMove() {
-  // AI's move logic
+  // get all empty cells in the grid
   const emptyCells = Array.from(gridItems).filter(
     (item) => item.innerText === ""
   );
   if (emptyCells.length > 0) {
+    // choose a random empty cell and set it to "O" for AI's move
     const randomCell =
       emptyCells[Math.floor(Math.random() * emptyCells.length)];
     randomCell.innerText = "O";
